128220c066a7ba0ebf1313eb03c7ca2d7ad2f193
ukrWily <ukrwily@gmail.com>
2026-01-28 19:53:38 +0200

    PageNav

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 
 function Homepage() {
   return (
     <div>
+      <PageNav />
       <h1>WorldWise</h1>
-      <Link to="/product">Product</Link>
-      <br />
       <Link to="/pricing">Pricing</Link>
     </div>
   );
