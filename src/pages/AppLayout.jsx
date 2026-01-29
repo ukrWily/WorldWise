2e1e09c415e93fe1d95ef1c3223c334a5ec7da4a
ukrWily <ukrwily@gmail.com>
2026-01-29 19:57:54 +0200

    AppNav

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,5 +1,12 @@
+import AppNav from "../components/AppNav.jsx";
+
 function AppLayout() {
-  return <div></div>;
+  return (
+    <div>
+      <AppNav />
+      <p>App</p>
+    </div>
+  );
 }
 
 export default AppLayout;
