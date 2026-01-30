3e0f386130da1c0b03277e30e33599c4f64e1474
ukrWily <ukrwily@gmail.com>
2026-01-30 18:33:06 +0200

    Starter

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Homepage from "./pages/Homepage.jsx";
 import Product from "./pages/product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
-import AppLayout from './pages/AppLayout'
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/app" element={<AppLayout />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
