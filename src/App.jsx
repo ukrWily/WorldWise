d1c312c5eadbc63da77b533b13042ecdb6b9a92d
ukrWily <ukrwily@gmail.com>
2026-02-03 20:19:02 +0200

    cityList

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import Pricing from "./pages/Pricing.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login.jsx";
+import CityList from "./components/CityList.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
-        <Route path="/app" element={<AppLayout />}></Route>
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
+          <Route path="countries" element={<div>Countries Page</div>} />
+          <Route path="form" element={<div>Form Page</div>} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
